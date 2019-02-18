801690ba0b64db02a6b7ce708638e5e3cd9c0dfc
Choleslaw <41842273+kymiddleton@users.noreply.github.com>
2019-02-18 18:18:57 -0500

    code cleanup and completed edit of readme file

---
+++ Readme.md
@@ -0,0 +1,26 @@
+# Todo List App
+
+The first phase of this project includes creating a todo list application with Node.js, Express and Express routes, and jQuery's AJAX method.
+
+Create a todo list application that matches the mobile and desktop designs below:
+
+![Desktop](public/images/desktop.png)
+
+![Mobile](public/images/mobile.png)
+
+Use [Font Awesome](https://fontawesome.com/) for the icons. The font used is Roboto from Google Fonts. 
+
+### Project must include the following requirements:
+
+- Use array methods instead of for loops
+- Make a GET route for getting all todo list items
+- Make a POST route for adding a new todo list item
+- Make a DELETE route for deleting a todo list item using the X button next to it
+- Make a PUT route for updating a todo list item when it is checked or unchecked
+- Modularize your routes by seperating your Express routes into a seperate route file
+- No global variables other than functions and state (front-end)
+- Use a single render function to render items to the page (front-end)
+- Use AJAX (part of the jQuery library) to make GET, POST, PUT, and DELETE requests from the front-end
+- Deploy the final application to Heroku.  (todo_application repo connected with todo-app-bootcamp)
+
+The second phase of the project will add a database to store todo entries even if the server application is reset. 
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -17,21 +17,21 @@ module.exports = function (app) {
         .catch(function (err) {
             res.json(err);
         });
-});
+  });
 
-// POST request: Route for saving a new Inventory entry to the database.
-app.post('/api/todo_list', function (req, res) {
-  db.Todo.create(req.body)
+  // POST request: Route for saving a new Inventory entry to the database.
+  app.post('/api/todo_list', function (req, res) {
+    db.Todo.create(req.body)
       .then(function (dbTodo) {
           res.json(dbTodo);
       })
       .catch(function (err) {
           res.json(err);
       });
-});
+  });
 
   // Route for saving updates 
-app.put('/api/todo_list', function (req, res) {
+  app.put('/api/todo_list', function (req, res) {
   // Find an entry by ID 
   db.Todo.findOneAndUpdate({itemID: req.body.itemID}, {$set: {itemCount: req.body.itemCount}})
       .then(function (dbTodo) {
@@ -40,35 +40,5 @@ app.put('/api/todo_list', function (req, res) {
       .catch(function(err) {
           res.json(err);
       });
-});
+  });
 };
-
-
-//routes from last week - no longer need for database
-
-  // GET Request
-//   app.get('/api/todo_list/:index', function (req, res) {
-//     res.json(taskList[req.params.index]);
-//   });
-
-//   // PUT Request: Responds with success: true or false if successful
-//   app.put('/api/todo_list', function (req, res) {
-//     for (let key in req.body) {
-//       if (!sampleTodo.hasOwnProperty(key)) {
-//         return res.json({ success: false });
-//       }
-//     }
-    
-//     // Ensure boolean values for item status
-//     req.body.todoStatus = req.body.todoStatus === 'true';
-//     taskList.splice(req.params.index, 1, req.body);
-//     res.json({ success: true });
-//   });
-
-//   // DELETE Request: Responds with success: true or false if successfu
-//   app.post('/api/delete/todo_list', function (req, res) {
-//     console.log(req.body);
-//     taskList.splice(req.body.index, 1);
-//     res.json(taskList);
-//   });
-// };
